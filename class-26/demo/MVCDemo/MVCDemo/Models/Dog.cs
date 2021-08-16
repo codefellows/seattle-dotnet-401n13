98fb1fec70cace93ecac6e95d4a6f1c69d46d5fb
John Cokos <highlander.44@gmail.com>
2021-08-16 12:26:00 -0700

    MVCDemo Posted

---
+++ class-26/demo/MVCDemo/MVCDemo/Models/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemo.Models
+{
+  public class Dog
+  {
+    public string Name { get; set; }
+    public string Breed { get; set; }
+    public int Age { get; set;  }
+  }
+}
